29a3b22d85a839c7ed52aea58df952693ba394c3
rari <suffocationanymore@gmail.com>
2021-11-08 11:01:35 +0300

    Верстка десктоп 4 блока

--- build/js/script.js
+++ build/js/script.js
@@ -1,5 +1,5 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
+/*var pageHeader = document.querySelector('.page-header');
 var headerToggle = document.querySelector('.page-header__toggle');
 
 pageHeader.classList.remove('page-header--nojs');
@@ -12,4 +12,28 @@ headerToggle.addEventListener('click', function () {
     pageHeader.classList.add('page-header--closed');
     pageHeader.classList.remove('page-header--opened');
   }
+});*/
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  spaceBetween: 30,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-next',
+    prevEl: '.swiper-prev',
+  },
+
 });
